9312dddcd75814ebdd91143d0cee8fd8b4bc3b07
Harsh-Manoor <harshmanoor@gmail.com>
2021-08-11 20:58:51 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import React from 'react';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Pages/Home';
-import About from './components/Pages/About';
-import Blog from './components/Pages/Blog';
-import Contact from './components/Pages/Contact';
-import SignIn from './components/Pages/SignIn';
+import './App.css';
 
 function App() {
   return (
     <>
-       <Router>
-          <NavBar/>
-          <div className='pages'>
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/about" component={About}/>
-              <Route exact path="/blog" component={Blog}/>
-              <Route exact path="/contact" component={Contact}/>
-              <Route exact path="/sign-in" component={SignIn}/>
-          </Switch>
-          </div>
-       </Router>
-       
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/'/>
+        </Switch>
+    </Router>
     </>
   );
 }
